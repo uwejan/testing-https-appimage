d8fd33273130455bc0948ba665ed9aacd5c62bf4
Saddam Uwejan <suwejan@consultpaysource.com>
2023-06-15 22:06:05 +0300

    init

---
+++ src/components/Greet.vue
@@ -0,0 +1,39 @@
+<template>
+    {{errMsg}}
+    <li v-for="(item, index) in posts">
+        {{item.id}}
+    </li>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import axios from "axios";
+
+export default defineComponent({
+    name: "Greet",
+    data(){
+        return {
+            errMsg: null,
+            posts: []
+        }
+    },
+    method:{
+        async getPosts() {
+
+        }
+
+    },
+    async mounted() {
+        try {
+            let res = await axios.get("https://jsonplaceholder.typicode.com/users/1/posts")
+            this.posts = res.data
+        }catch (e) {
+            this.errMsg = e
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
